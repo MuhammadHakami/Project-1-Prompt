8a99a04225eaa2e31563f98fcfea4f0e3c3cbb07
MuhammadHakami <mmm.mephiles@gmail.com>
2019-03-11 16:48:40 +0300

    finished the game

--- js/main.js
+++ js/main.js
@@ -1,27 +1,72 @@
 var search=document.querySelectorAll('.row div');
-var reset=document.querySelector('button');
-var counter=1; 
+var resButton=document.querySelector('button');
+var turn=0;
+var score={1:0,2:0};
+var stateSpace={0:'',1:'',2:'',3:'',4:'',5:'',6:'',7:'',8:''};
+var winningStates=[0,3];
 
-reset.addEventListener('click',function(event){
+function reset(event){
+    turn=0
     event.preventDefault()
     for (i=0;i<9;i++){
-        search[i].style.backgroundColor=null
+        search[i].style.backgroundImage=null
     }
-})
+}
+
+resButton.addEventListener('click',reset);
+
+function remove(){
+    for (i=0;i<9;i++){
+    search[i].removeEventListener('click',why)
+    }
+}
 
 function run(){
     function step(i){
     search[i].addEventListener('click',function why(){
-        if (search[i].style.backgroundColor){
+        if (search[i].style.backgroundImage){
             console.log('Chosen block')
         } else {
-            if (counter%2==0){
-                search[i].style.backgroundColor='blue';
+            if (turn%2==0){
+                search[i].style.backgroundImage='url(/home/shadows/Documents/wdi4/Project-1-Prompt/assets/x.jpg)';
+                search[i].style.backgroundPosition= 'center';
+                search[i].style.backgroundSize='cover';
             } else {
-                search[i].style.backgroundColor='red';
+                search[i].style.backgroundImage='url(/home/shadows/Documents/wdi4/Project-1-Prompt/assets/o.jpeg)';
+                search[i].style.backgroundPosition='center';
+                search[i].style.backgroundSize='cover';
             }
-            counter+=1;
-            console.log(counter);
+            console.log(turn);
+            turn+=1;
+        }
+        for (k=0;k<9;k++){
+            if (search[k].style.backgroundImage){
+            stateSpace[k]=search[k].style.backgroundImage
+            } else {
+                stateSpace[k]='None'
+            }
+        }
+        //console.log(stateSpace)
+        function match(){
+            player=(turn%2===0)?2:1;
+            score[player]+=1
+            swal(`Player ${player} Won!`, `${score[1]} VS ${score[2]}`, "success");
+            document.querySelector('.swal-modal .swal-button-container button').addEventListener('click',reset)
+        }
+        if ('None'!==stateSpace[0] & stateSpace[0]===stateSpace[1] & stateSpace[0]===stateSpace[2]){
+            match()
+        } else if ('None'!==stateSpace[0] & stateSpace[0]===stateSpace[3] & stateSpace[0]===stateSpace[6]){
+            match()
+        } else if ('None'!==stateSpace[0] & stateSpace[0]===stateSpace[4] & stateSpace[0]===stateSpace[8]){
+            match()
+        } else if ('None'!==stateSpace[4] & stateSpace[4]===stateSpace[1] & stateSpace[4]===stateSpace[7]){
+            match()
+        } else if ('None'!==stateSpace[4] & stateSpace[4]===stateSpace[3] & stateSpace[4]===stateSpace[5]){
+            match()
+        } else if ('None'!==stateSpace[8] & stateSpace[8]===stateSpace[7] & stateSpace[8]===stateSpace[6]){
+            match()
+        } else if ('None'!==stateSpace[8] & stateSpace[8]===stateSpace[5] & stateSpace[8]===stateSpace[2]){
+            match()
         }
         })
     }
@@ -30,4 +75,8 @@ function run(){
     }
 }
 
+
+
 run();
+
+
