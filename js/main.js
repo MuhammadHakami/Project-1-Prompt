28fe3334227f9ff2014ebc1953b36851e273a0c2
MuhammadHakami <mmm.mephiles@gmail.com>
2019-03-13 15:58:22 +0300

    finished the game

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ var turn=0;
 var score={1:0,2:0};
 var stateSpace={0:'',1:'',2:'',3:'',4:'',5:'',6:'',7:'',8:''};
 var winningStates=[0,3];
+var playerx=document.querySelector('#playerx');
+var playero=document.querySelector('#playero');
 
 function reset(event){
     //document.querySelector('.playerx').innerHTML(`Player X: Scored ${score[1]}`)
@@ -13,7 +15,46 @@ function reset(event){
     for (i=0;i<9;i++){
         search[i].style.backgroundImage=null;
         search[i].style.backgroundPosition='left';
+        stateSpace={0:'',1:'',2:'',3:'',4:'',5:'',6:'',7:'',8:''};
         search[i].className='shake-slow';
+        playerx.textContent="Player X: Scored "+score[1];
+        playero.textContent="Player O: Scored "+score[2];
+    }
+}
+
+function checkWinningReq(stateSpace,search){
+    for (k=0;k<9;k++){
+        if (search[k].style.backgroundImage){
+        stateSpace[k]=search[k].style.backgroundImage
+        } else {
+            stateSpace[k]=0
+        }
+    }
+
+    function match(j){
+        player=(stateSpace[j]==='url("./assets/x.jpg")')?1:2;
+        score[player]+=1
+        swal(`Player ${player} Won!`, `${score[1]} VS ${score[2]}`, "success");
+        document.querySelector('.swal-modal .swal-button-container button').addEventListener('click',reset)
+        document.querySelector('body > div').addEventListener('click',reset)
+        turn=0
+    }
+    if (0!==stateSpace[0] & stateSpace[0]===stateSpace[1] & stateSpace[0]===stateSpace[2]){
+        match(0)
+    } else if (0!==stateSpace[0] & stateSpace[0]===stateSpace[3] & stateSpace[0]===stateSpace[6]){
+        match(0)
+    } else if (0!==stateSpace[0] & stateSpace[0]===stateSpace[4] & stateSpace[0]===stateSpace[8]){
+        match(0)
+    } else if (0!==stateSpace[4] & stateSpace[4]===stateSpace[1] & stateSpace[4]===stateSpace[7]){
+        match(4)
+    } else if (0!==stateSpace[4] & stateSpace[4]===stateSpace[3] & stateSpace[4]===stateSpace[5]){
+        match(4)
+    } else if (0!==stateSpace[4] & stateSpace[4]===stateSpace[2] & stateSpace[4]===stateSpace[6]){
+        match(4)
+    } else if (0!==stateSpace[8] & stateSpace[8]===stateSpace[7] & stateSpace[8]===stateSpace[6]){
+        match(8)
+    } else if (0!==stateSpace[8] & stateSpace[8]===stateSpace[5] & stateSpace[8]===stateSpace[2]){
+        match(8)
     }
 }
 
@@ -33,52 +74,30 @@ function run(){
         if (search[i].style.backgroundImage){
             console.log('Chosen block')
         } else {
-            if (turn%2==0){
-                search[i].style.backgroundImage='url(./assets/x.jpg)';
-                search[i].style.backgroundPosition= 'center';
-                search[i].style.backgroundSize='cover';
-                search[i].className=null;
-            } else {
-                search[i].style.backgroundImage='url(./assets/o.jpeg)';
-                search[i].style.backgroundPosition='center';
-                search[i].style.backgroundSize='cover';
-                search[i].className=null;
+            search[i].style.backgroundImage='url(./assets/x.jpg)';
+            search[i].style.backgroundPosition= 'center';
+            search[i].style.backgroundSize='cover';
+            search[i].className=null;
+            stateSpace[i]=search[i].style.backgroundImage;
+            let placeHolder=[];
+            for (let key in stateSpace){
+                if (!stateSpace[key] & key !== i){
+                    placeHolder.push(key);
+                }
             }
-            console.log(turn);
-            turn+=1;
-        }
-        for (k=0;k<9;k++){
-            if (search[k].style.backgroundImage){
-            stateSpace[k]=search[k].style.backgroundImage
-            } else {
-                stateSpace[k]='None'
+            // opponent
+            let n=placeHolder[Math.floor(Math.random()*placeHolder.length)];
+            console.log(i+"|||"+placeHolder);
+            (n===undefined)?reset(event):1;
+            if (n!==undefined){
+            search[n].style.backgroundImage='url(./assets/o.jpeg)';
+            search[n].style.backgroundPosition='center';
+            search[n].style.backgroundSize='cover';
+            search[n].className=null;
+            checkWinningReq(stateSpace,search);
             }
         }
         //console.log(stateSpace)
-        function match(){
-            player=(turn%2===0)?2:1;
-            score[player]+=1
-            swal(`Player ${player} Won!`, `${score[1]} VS ${score[2]}`, "success");
-            document.querySelector('.swal-modal .swal-button-container button').addEventListener('click',reset)
-            document.querySelector('body > div').addEventListener('click',reset)
-        }
-        if ('None'!==stateSpace[0] & stateSpace[0]===stateSpace[1] & stateSpace[0]===stateSpace[2]){
-            match()
-        } else if ('None'!==stateSpace[0] & stateSpace[0]===stateSpace[3] & stateSpace[0]===stateSpace[6]){
-            match()
-        } else if ('None'!==stateSpace[0] & stateSpace[0]===stateSpace[4] & stateSpace[0]===stateSpace[8]){
-            match()
-        } else if ('None'!==stateSpace[4] & stateSpace[4]===stateSpace[1] & stateSpace[4]===stateSpace[7]){
-            match()
-        } else if ('None'!==stateSpace[4] & stateSpace[4]===stateSpace[3] & stateSpace[4]===stateSpace[5]){
-            match()
-        } else if ('None'!==stateSpace[4] & stateSpace[4]===stateSpace[2] & stateSpace[4]===stateSpace[6]){
-            match()
-        } else if ('None'!==stateSpace[8] & stateSpace[8]===stateSpace[7] & stateSpace[8]===stateSpace[6]){
-            match()
-        } else if ('None'!==stateSpace[8] & stateSpace[8]===stateSpace[5] & stateSpace[8]===stateSpace[2]){
-            match()
-        }
         })
     }
     for (i=0;i<9;i++){
